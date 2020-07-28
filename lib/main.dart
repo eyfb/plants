c8b95df1f2b86cf1c2e559c8962f71445cbf7735
Emily <emily.yf.bao@gmail.com>
2020-07-28 16:15:17 -0600

    Got rid of  shared preferences dependancy and added path provider dependancy

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MyApp());
